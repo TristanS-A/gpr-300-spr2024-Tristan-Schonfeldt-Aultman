25b708dbf9d5b295da17efc5be2eb79234f008c7
Draginalo <tropicaltisa@gmail.com>
2025-02-17 20:59:20 -0500

    Added assignment 2.

---
+++ assignments/assignment2/assets/blinnPhong.frag
@@ -0,0 +1,93 @@
+#version 450
+
+in vec3 out_Normal;
+in Surface 
+{
+	vec3 worldPos;
+	vec3 worldNormal;
+	vec4 lightPos;
+	vec2 texcoord;
+	mat3 TBN_Mat;
+} fs_surface;
+
+struct Material 
+{
+	float ambientK;
+	float diffuseK;
+	float specularK;
+	float shininess;
+};
+
+struct Light 
+{
+	vec3 color;
+	vec3 pos;
+};
+
+out vec4 FragColor;
+
+uniform sampler2D _MainTex;
+uniform sampler2D _NormalMap;
+uniform sampler2D _ShadowMap;
+uniform vec3 _EyePos;
+uniform Material _Material;
+uniform Light _Light;
+
+//Light source
+uniform vec3 _LightDir = vec3(0.0, 1.0, 0.0);
+uniform vec3 _CamPos = vec3(1.0, 1.0, 1.0);
+uniform vec3 _AmbientColor = vec3(0.3, 0.4, 0.46);
+uniform bool _Use_NormalMap;
+
+float shadowCalculations(vec4 fragPosLightSpace)
+{
+	//Perspective devide -> normalized device coords
+	vec3 projectionCoords = fragPosLightSpace.xyz / fragPosLightSpace.w;
+	
+	//Map to 0-1
+	projectionCoords = (projectionCoords * 0.5) + 0.5;
+
+	////Way to get rid of plane out of camera issues for shadows
+	//if (projectionCoords.z <= 0.0 || projectionCoords.z >= 1.0)
+	//{
+	//	return 0.0f;
+	//}
+
+	float closestDepth = texture(_ShadowMap, projectionCoords.xy).r;
+	float currentDepth = projectionCoords.z;
+
+	float bias = 0.0; //MAKE THIS A UNIFORM
+	float shadow = ((currentDepth - bias) > closestDepth) ? 1.0 : 0.0;
+
+	return shadow;
+}
+
+vec3 blinnPhong(vec3 normal, vec3 fragPos)
+{
+	//Normalize inputs
+	vec3 viewDir = normalize(_CamPos - fragPos);
+	vec3 lightDir = normalize(_Light.pos - fragPos);
+	vec3 halfDir = normalize(_Light.pos + viewDir);
+
+	//Dot products
+	float nDotL = max(dot(normal, lightDir), 0.0);
+	float nDotH = max(dot(normal, halfDir), 0.0);
+
+	vec3 diffuse = vec3(nDotL * _Material.diffuseK);
+	vec3 specular = vec3(pow(nDotH, _Material.shininess * 128.0) * _Material.specularK);
+
+	return (diffuse + specular);
+}
+
+void main() 
+{
+	vec3 normal = normalize(fs_surface.worldNormal);
+	float shadow = shadowCalculations(fs_surface.lightPos);
+	
+	vec3 lighting = blinnPhong(normal, fs_surface.worldPos);
+	lighting *= (1.0 - shadow);
+	lighting *= _Light.color * _AmbientColor * _Material.ambientK;
+
+	vec3 objectColor = texture(_MainTex, fs_surface.texcoord).rgb;
+	FragColor = vec4(objectColor * lighting, 1.0);
+}
---
+++ assignments/assignment2/main.cpp
@@ -0,0 +1,292 @@
+#include <stdio.h>
+#include <math.h>
+
+#include <ew/external/glad.h>
+
+#include <GLFW/glfw3.h>
+#include <imgui.h>
+#include <imgui_impl_glfw.h>
+#include <imgui_impl_opengl3.h>
+
+#include <ew/shader.h>
+#include <ew/model.h>
+#include <ew/camera.h>
+#include <ew/cameraController.h>
+#include <ew/transform.h>
+#include <ew/texture.h>
+
+#include <ew/procGen.h>
+#include <glm/gtx/transform.hpp>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+
+void framebufferSizeCallback(GLFWwindow* window, int width, int height);
+GLFWwindow* initWindow(const char* title, int width, int height);
+void drawUI();
+
+//Global state
+int screenWidth = 1080;
+int screenHeight = 720;
+float prevFrameTime;
+float deltaTime;
+
+struct Material
+{
+	float ambientK = 1.0;
+	float diffuseK = 0.5;
+	float specularK = 0.5;
+	float shininess = 128;
+	char* name = "Default";
+};
+
+enum Materials {
+	METAL,
+	ROCK,
+	PLASTIC
+};
+
+struct DepthBuffer
+{
+	GLuint fbo;
+	GLuint depth;
+
+	void init()
+	{
+		//Bind framebuffer
+		glGenFramebuffers(1, &fbo);
+		glBindFramebuffer(GL_FRAMEBUFFER, fbo);
+
+		//Create depth texture attachment
+		glGenTextures(1, &depth);
+		glBindTexture(GL_TEXTURE_2D, depth);
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_DEPTH_COMPONENT,
+			256, 256, 0, GL_DEPTH_COMPONENT, GL_FLOAT, NULL);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+
+		//Bind depth texture attachment
+		glBindFramebuffer(GL_FRAMEBUFFER, depth);
+		glFramebufferTexture2D(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_TEXTURE_2D, depth, 0);
+		glDrawBuffer(GL_NONE);
+		glReadBuffer(GL_NONE);
+
+		//Check if frame buffer was created
+		if (glCheckFramebufferStatus(GL_FRAMEBUFFER) != GL_FRAMEBUFFER_COMPLETE)
+		{
+			printf("Failed to bind framebuffer");
+		}
+
+		glBindFramebuffer(GL_FRAMEBUFFER, 0);
+	}
+} depthBuffer;
+
+//Caching things
+ew::Camera camera;
+ew::CameraController camController;
+ew::Transform suzanneTransform;
+Material mats[3] = { 
+	{1.0, 0.6, 1.0, 30, "Metal"},
+	{1.0, 0.8, 0.3, 128, "Rock"},
+	{1.0, 0.8, 0.4, 30, "Plastic"}
+};
+short matIndex = 0;
+Material* currMat = &mats[matIndex];
+bool usingNormalMap = true;
+
+ew::Mesh plane;
+glm::vec3 lightVec = { 2.0f, 2.0f, -2.0f };
+
+void render(ew::Shader shader, ew::Shader shadowShader, ew::Model &model, ew::Transform &modelTransform, GLint tex, GLint normalMap, const float dt)
+{
+	//Pipeline defenitions
+	glEnable(GL_CULL_FACE);
+	glCullFace(GL_BACK);  //MAKE THIS IMGUI TOGGLE ABLE FOR SELF SHADOW ISSUES (CULL FRONT). ALSO DO PCF
+	glEnable(GL_DEPTH_TEST);
+
+	//Shadow pass
+	glBindFramebuffer(GL_FRAMEBUFFER, depthBuffer.fbo);
+	{
+		//Begin pass
+		glViewport(0, 0, 256, 256);
+
+		//GFX Pass
+		glClear(GL_DEPTH_BUFFER_BIT);
+
+		//Light's view of the scene
+		const auto lightProj = glm::ortho(-10.0f, 10.0f, -10.0f, 10.0f, 0.1f, 100.0f);
+		const auto lightView = glm::lookAt(lightVec, glm::vec3(0.0f), glm::vec3(0.0f, -1.0f, 0.0f));
+		const auto lightViewProj = lightProj * lightView;
+
+		shadowShader.use();
+		shadowShader.setMat4("_Model", modelTransform.modelMatrix());
+		shadowShader.setMat4("_LightViewProj", lightViewProj);
+	
+		model.draw();
+	}
+	glBindFramebuffer(GL_FRAMEBUFFER, 0);
+
+	//Render lighting
+	{
+		//Begin pass
+		glViewport(0, 0, screenWidth, screenHeight);
+
+		//GFX Pass
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+		glClearColor(0.6f, 0.8f, 0.92f, 1.0f);
+
+		glActiveTexture(GL_TEXTURE0);
+		glBindTexture(GL_TEXTURE_2D, tex);
+
+		glActiveTexture(GL_TEXTURE1);
+		glBindTexture(GL_TEXTURE_2D, normalMap);
+
+		glActiveTexture(GL_TEXTURE2);
+		glBindTexture(GL_TEXTURE_2D, depthBuffer.depth);
+
+		shader.use();
+		modelTransform.rotation = glm::rotate(modelTransform.rotation, dt, glm::vec3(0.0, 1.0, 0.0));
+		shader.setMat4("_Model", modelTransform.modelMatrix());
+		shader.setMat4("camera_viewProj", camera.projectionMatrix() * camera.viewMatrix());
+		shader.setInt("_MainTex", 0);
+		shader.setInt("_NormalMap", 1);
+		shader.setInt("_ShadowMap", 2);
+		shader.setBool("_Use_NormalMap", usingNormalMap);
+
+		shader.setVec3("_CamPos", camera.position);
+		shader.setVec3("_Light.color", glm::vec3(1.0f, 0.0f, 1.0f));
+		shader.setVec3("_Light.pos", glm::vec3(lightVec));
+
+		shader.setFloat("_Material.ambientK", currMat->ambientK);
+		shader.setFloat("_Material.diffuseK", currMat->diffuseK);
+		shader.setFloat("_Material.specularK", currMat->specularK);
+		shader.setFloat("_Material.shininess", currMat->shininess);
+
+		model.draw();
+
+		//Light's view of the scene
+		shader.setMat4("_Model", glm::translate(glm::vec3(0.0f, -2.0f, 0.0f)));
+		plane.draw();
+	}
+}
+
+int main() {
+	GLFWwindow* window = initWindow("Assignment 2", screenWidth, screenHeight);
+	glfwSetFramebufferSizeCallback(window, framebufferSizeCallback);
+
+	camera.position = { 0.0f, 0.0f, 5.0f };
+	camera.target = { 0.0f, 0.0f, 0.0f };
+	camera.aspectRatio = (float)screenWidth / screenHeight;
+	camera.fov = 60.0f;
+
+	ew::Shader lit_Shader = ew::Shader("assets/blinnPhong.vert", "assets/blinnPhong.frag");
+	ew::Shader shadowShader = ew::Shader("assets/shadow.vert", "assets/shadow.frag");
+
+	ew::Model suzanne = ew::Model("assets/suzanne.obj");
+
+	GLint Rock_Color = ew::loadTexture("assets/Rock_Color.png");
+	GLint rockNormal = ew::loadTexture("assets/Rock_Normal.png");
+
+	plane.load(ew::createPlane(10, 10, 10));
+
+	depthBuffer.init();
+
+	while (!glfwWindowShouldClose(window)) {
+		glfwPollEvents();
+
+		float time = (float)glfwGetTime();
+		deltaTime = time - prevFrameTime;
+		prevFrameTime = time;
+
+		//RENDER
+		camController.move(window, &camera, deltaTime);
+		render(lit_Shader, shadowShader, suzanne, suzanneTransform, Rock_Color, rockNormal, deltaTime);
+		drawUI();
+
+		glfwSwapBuffers(window);
+	}
+	printf("Shutting down...");
+}
+
+
+void drawUI() {
+	ImGui_ImplGlfw_NewFrame();
+	ImGui_ImplOpenGL3_NewFrame();
+	ImGui::NewFrame();
+
+	ImGui::Begin("Settings");
+	if (ImGui::BeginCombo("Select Material", currMat->name))
+	{
+		for (int i = 0; i < sizeof(mats) / sizeof(mats[0]); i++)
+		{
+			bool is_selected = (currMat->name == mats[i].name);
+			if (ImGui::Selectable(mats[i].name, is_selected))
+			{
+				currMat = &mats[i];
+			}
+		}
+		ImGui::EndCombo();
+	}
+	if (ImGui::CollapsingHeader("Edit Materials"))
+	{
+		for (int i = 0; i < sizeof(mats) / sizeof(mats[0]); i++)
+		{
+			if (ImGui::CollapsingHeader(mats[i].name))
+			{
+				ImGui::SliderFloat("AmbientK", &mats[i].ambientK, 0.0, 1.0);
+				ImGui::SliderFloat("DiffuseK", &mats[i].diffuseK, 0.0, 1.0);
+				ImGui::SliderFloat("SpecularK", &mats[i].specularK, 0.0, 1.0);
+				ImGui::SliderFloat("Shininess", &mats[i].shininess, 2.0, 1024.0);
+			}
+		}
+	}
+	ImGui::Image((ImTextureID)(intptr_t)depthBuffer.depth, ImVec2(256, 256));
+	ImGui::End();
+
+	ImGui::Render();
+	ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+}
+
+void framebufferSizeCallback(GLFWwindow* window, int width, int height)
+{
+	glViewport(0, 0, width, height);
+	screenWidth = width;
+	screenHeight = height;
+}
+
+/// <summary>
+/// Initializes GLFW, GLAD, and IMGUI
+/// </summary>
+/// <param name="title">Window title</param>
+/// <param name="width">Window width</param>
+/// <param name="height">Window height</param>
+/// <returns>Returns window handle on success or null on fail</returns>
+GLFWwindow* initWindow(const char* title, int width, int height) {
+	printf("Initializing...");
+	if (!glfwInit()) {
+		printf("GLFW failed to init!");
+		return nullptr;
+	}
+
+	GLFWwindow* window = glfwCreateWindow(width, height, title, NULL, NULL);
+	if (window == NULL) {
+		printf("GLFW failed to create window");
+		return nullptr;
+	}
+	glfwMakeContextCurrent(window);
+
+	if (!gladLoadGL(glfwGetProcAddress)) {
+		printf("GLAD Failed to load GL headers");
+		return nullptr;
+	}
+
+	//Initialize ImGUI
+	IMGUI_CHECKVERSION();
+	ImGui::CreateContext();
+	ImGui_ImplGlfw_InitForOpenGL(window, true);
+	ImGui_ImplOpenGL3_Init();
+
+	return window;
+}
+
